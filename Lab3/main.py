c583884f107cea130279510cce079d416ef60ff3
Pode98 <alessio.podesta@studenti.polito.it>
2021-09-18 12:22:40 +0200

    Lab3 done

--- Lab3/main.py
+++
@@ -1,5 +0,0 @@
-import json
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-from acipy.constants import c
---
+++ Lab3/main1.py
@@ -0,0 +1,41 @@
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.constants import c
+import pandas as pd
+from Lab3.Node import *
+from Lab3.Line import *
+from Lab3.Signal_information import *
+from Lab3.Network import *
+
+network = Network('nodes.json')
+network.connect()
+node_labels = network.nodes.keys()
+pairs = []
+for label1 in node_labels:
+    for label2 in node_labels:
+        if label1!=label2:
+            pairs.append(label1+label2)
+
+columns = ['path','latency','noise','snr']
+df = pd.DataFrame()
+paths = []
+latencies = []
+noises = []
+snrs = []
+for pair in pairs:
+    for path in network.find_paths(pair[0],pair[1]):
+        path_string = ''
+        for node in path:
+            path_string += node + '->'
+        paths.append(path_string[:-2])
+# Propagation
+signal_information = SignalInformation(1,path)
+signal_information = network.propagate(signal_information)
+latencies.append(signal_information.latency)
+noises.append(signal_information.noise_power)
+snrs.append(10*np.log10( signal_information.signal_power/signal_information.noise_power) )
+df['path'] = paths
+df['latency'] = latencies
+df['noise'] = noises
+df['snr'] = snrs
